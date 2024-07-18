5194b0eb01efe905b0f16e78dd10130296dff161
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2024-07-18 12:13:48 +0300

    student function updated

--- netlify/functions/student.js
+++ netlify/functions/student.js
@@ -1,11 +1,23 @@
 exports.handler = async function(event, context) {
     const params = event.queryStringParameters;
-    const name = params.name;
+    const url = params.url;
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: `Hello ${name}` }),
-      };
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+    
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data),
+        };
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+    
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: "Failed to fetch data" }),
+        };
+    }
 
 };
   
